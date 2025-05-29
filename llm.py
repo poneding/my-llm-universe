31b05f02fa9a8fae6f878ccb116ab0f5aaf5b4c3
poneding <poneding@gmail.com>
2025-05-29 11:07:50 +0800

    feat: support zhipuai chat model

--- llm.py
+++ llm.py
@@ -3,6 +3,7 @@ import os
 from dotenv import find_dotenv, load_dotenv
 from langchain_chroma import Chroma
 from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
+from langchain_zhipuai_dev.chat import ChatZhipuAI
 from pydantic import SecretStr
 from zhipuai import ZhipuAI
 
@@ -29,13 +30,13 @@ def get_embedding():
 
 def get_chat_model():
     # Google Generative AI Chat Model
-    return ChatGoogleGenerativeAI(
-        google_api_key=SecretStr(GOOGLE_API_KEY) if GOOGLE_API_KEY else None,
-        model="gemini-2.0-flash",
-        temperature=0,
-    )
+    # return ChatGoogleGenerativeAI(
+    #     google_api_key=SecretStr(GOOGLE_API_KEY) if GOOGLE_API_KEY else None,
+    #     model="gemini-2.0-flash",
+    #     temperature=0,
+    # )
 
     # ZhipuAI Chat Model，不支持 RunnableParallel 运算
-    # return ZhipuAI(
-    #     api_key=ZHIPUAI_API_KEY,
-    # )
+    return ChatZhipuAI(
+        api_key=os.getenv("ZHIPUAI_API_KEY"), model="glm-4", temperature=0
+    )
